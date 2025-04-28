dbb7879ab45a66d555b650de1449bc70f229282a
elec <adrielclinton@gmail.com>
2025-04-28 22:58:12 +0530

    Menu init

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MenuManager : MonoBehaviour
+{
+    //public IPanel GameMenuPanel;
+    public GameObject GameMenuPanel;
+    public GameObject AudioSourceParent;
+    //void 
+
+    private void Start()
+    {
+        GameMenuPanel.SetActive(true);
+        AudioSourceParent.SetActive(false);
+        Time.timeScale = 0f;
+    }
+
+    public void GameStart()
+    {
+        GameMenuPanel.SetActive(false);
+        AudioSourceParent.SetActive(true);
+        Time.timeScale = 1f;
+    }
+}
